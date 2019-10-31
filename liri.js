c7f6e00791d24c11736d0855768c21f4a657f2b8
Kaiisha05 <Kaiisha@yahoo.com>
2019-10-31 12:14:36 -0400

    omdb api working, spotify function added adn variables set. Need to correct issues with the client secret and ID

--- liri.js
+++ liri.js
@@ -1,37 +1,49 @@
+require("dotenv").config();
 var axios = require("axios");
 var fs = require("fs");
+var keys = require("./keys.js");
 // read & set environmental variables with the dotenv package
-require("dotenv").config();
+
 
 // Able to access my keys informaiton
-//   var spotify = new Spotify(keys.spotify);
+var Spotify = require("node-spotify-api");
+
+var spotify = new Spotify(keys.spotify);
+
+var songAPiI = function(song){
+  spotify.search({ 
+    type: 'track', 
+    query: song,
+    limit: 1
+  }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    console.log(data)
+  })
+};
 
 
 
-//  9. Make it so liri.js can take in one of the following commands:
-
-
-var searchMovie = function() {
+var searchMovie = function(movie) {
   // divider will be used as a spacer between the tv data we print in log.txt
   var divider = "\n------------------------------------------------------------\n\n";
 
-  //    * `movie-this`
-  this.findMovie= function(movie) {
-    var URL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    var movieURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-    axios.get(URL).then(function(response) {
+    axios.get(movieURL).then(function(response) {
       // Place the response.data into a variable, jsonData.
       var jsonData = response.data;
   
       var showData = [
         "Title: " + jsonData.Title,
-        "Release Date: " + jsonData.Year.join(", "),
+        "Release Date: " + jsonData.Year,
         "Rating: " + jsonData.Rated,
-        "Rotten Tomatoes Rating: " + jsonData.Ratings[1].source, 
+        "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value, 
         "Country Produce: " + jsonData.Country,
         "Language:" + jsonData.Language,
         "Plot:" + jsonData.Plot,
-        "Actors:" + jsonData.Actors.join(", ")
+        "Actors:" + jsonData.Actors
       ].join("\n\n");
 
       // Append showData and the divider to log.txt, print showData to the console
@@ -40,17 +52,32 @@ var searchMovie = function() {
         console.log(showData);
       });
     });
-  };
+  
 
 
 
 };
-searchMovie();
+var api = process.argv[2];
+console.log(process.argv[2])
+
+var userInput = process.argv[3];
+console.log(userInput)
+
+
+if (api === `movie-this`) {
+  searchMovie(userInput)
+}
+
+else if (api === `spotify-this-song`) {
+  songAPiI(userInput)
+}
+// searchMovie();
+
 
 
 //    * `concert-this`
 
-//    * `spotify-this-song`
+//    * 
 
 
 
