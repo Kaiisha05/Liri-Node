4ede24e3f4f2c6d3854259c8019468a47ebb7adb
Kaiisha05 <Kaiisha@yahoo.com>
2019-11-02 13:39:59 -0400

    corrected spotify response. function updated to allow multi word input from user.

--- liri.js
+++ liri.js
@@ -11,25 +11,50 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
-var songAPiI = function(song){
+var songAPI = function(song){
   spotify.search({ 
     type: 'track', 
     query: song,
     limit: 1
   }, 
-   var musicData = [
-     
-   ]
+  //  var musicData = [
+  //    "Artist: " + ,
+  //    "Song's Name: " + ,
+  //    "Link: " + ,
+  //    "Album: " + ,
+  //  ].join("\n\n");
   
   function(err, data) {
     if (err) {
-      return console.log('Error occurred: ' + err);
+      console.log('Error occurred: ' + err);
+      return;
     }
-    console.log(data)
+    console.log(data.tracks.items)
   })
 };
 
+var concertSearch = function(concert) {
+  var divider = "\n------------------------------------------------------------\n\n";
 
+  var concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+  axios.get(concertURL).then(function(response) {
+console.log(response)
+    var concertData = response.data[0];
+
+    var concertData  = [
+      "Venue: " + concertData.offers.venue.name ,
+      "Location: " + concertData.offers.venue.city,
+      "Date: " + concertData.datetime,
+    ].join("\n\n");
+
+    fs.appendFile("log.txt" , concertData + divider,
+    function(err) {
+      if (err) throw err;
+        console.log(concertData);
+    })
+  })
+};
 
 var searchMovie = function(movie) {
   // divider will be used as a spacer between the tv data we print in log.txt
@@ -47,27 +72,36 @@ var searchMovie = function(movie) {
         "Rating: " + jsonData.Rated,
         "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value, 
         "Country Produce: " + jsonData.Country,
-        "Language:" + jsonData.Language,
-        "Plot:" + jsonData.Plot,
-        "Actors:" + jsonData.Actors
+        "Language: " + jsonData.Language,
+        "Plot: " + jsonData.Plot,
+        "Actors: " + jsonData.Actors
       ].join("\n\n");
 
       // Append showData and the divider to log.txt, print showData to the console
-      fs.appendFile("random.txt", movieData + divider, function(err) {
+      fs.appendFile("log.txt", movieData + divider, function(err) {
         if (err) throw err;
         console.log(movieData);
       });
     });
-  
-
-
-
 };
+
+
 var command = process.argv[2];
 console.log("Input Command:" + command)
 
-var userInput = process.argv[3];
-console.log("User Input: " + userInput)
+var userInput = process.argv.slice(3).join(" ");
+// console.log("User Input: " + userInput)
+
+// switch (command) {
+//   case `movie-this`:
+//     text = userInput;
+//     break;
+//     case `spotify-this-song`:
+//       text = userInput;
+//       break;
+// }
+
+
 
 
 if (command === `movie-this`) {
@@ -75,16 +109,13 @@ if (command === `movie-this`) {
 }
 
 else if (command === `spotify-this-song`) {
-  songAPiI(userInput)
+  songAPI(userInput)
 }
-// searchMovie();
+
+// if (command === `concert-this`) {
+//   concertSearch(userInput)
+// };
 
 
 
-//    * `concert-this`
 
-//    * 
-
-
-
-//    * `do-what-it-says`*/
