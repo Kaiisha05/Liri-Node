5e7a890011ed8a7a4a0244b65db522ac1ea11141
Kaiisha05 <Kaiisha@yahoo.com>
2019-11-04 10:02:25 -0500

    added description of app to ReadMe

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Liri-Node
 
+App will search Spotify, Bands In Town and OMDB.
+
+User will be able to input a track name and the Spotify API will return:
+Artist(s) name, Name of the song, link to a clip of the song and the name of the album.
+
+When user inputs the name of a movie axios combined with the OMDB API will return:
+Title, Release Date, Rating, Rotten Tomatoes Rating, Country it was produced in, Language, Plot and Actor(s).
+
+When used inputs the name of an artist/band. The Bands In Town along with axios will return;
+Venue, location adn venue.
+
+
+
+
 1. Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
 2. Give a high-level overview of how the app is organized
 3. Give start-to-finish instructions on how to run the app
--- liri.js
+++ liri.js
@@ -11,78 +11,80 @@ var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
-var songAPI = function(song){
-  spotify.search({ 
-    type: 'track', 
+var songAPI = function (song) {
+  spotify.search({
+    type: 'track',
     query: song,
     limit: 1
-  }, 
-  //  var musicData = [
-  //    "Artist: " + ,
-  //    "Song's Name: " + ,
-  //    "Link: " + ,
-  //    "Album: " + ,
-  //  ].join("\n\n");
-  
-  function(err, data) {
-    if (err) {
-      console.log('Error occurred: ' + err);
-      return;
-    }
-    console.log(data.tracks.items)
-  })
+  },
+    //  var musicData = [
+    //    "Artist: " + ,
+    //    "Song's Name: " + ,
+    //    "Link: " + ,
+    //    "Album: " + ,
+    //  ].join("\n\n");
+
+    function (err, data) {
+      if (err) {
+        console.log('Error occurred: ' + err);
+        return;
+      }
+      console.log(data.tracks.items)
+    })
 };
 
-var concertSearch = function(concert) {
+var concertSearch = function (concert) {
   var divider = "\n------------------------------------------------------------\n\n";
 
   var concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-  axios.get(concertURL).then(function(response) {
-console.log(response)
+  axios.get(concertURL).then(function (response) {
+    console.log(response)
     var concertData = response.data[0];
 
-    var concertData  = [
-      "Venue: " + concertData.offers.venue.name ,
+    var concertData = [
+      "Venue: " + concertData.offers.venue.name,
       "Location: " + concertData.offers.venue.city,
       "Date: " + concertData.datetime,
     ].join("\n\n");
 
-    fs.appendFile("log.txt" , concertData + divider,
-    function(err) {
-      if (err) throw err;
+    fs.appendFile("log.txt", concertData + divider,
+      function (err) {
+        if (err) throw err;
         console.log(concertData);
-    })
+      })
   })
 };
 
-var searchMovie = function(movie) {
+var searchMovie = function (movie) {
   // divider will be used as a spacer between the tv data we print in log.txt
   var divider = "\n------------------------------------------------------------\n\n";
 
-    var movieURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+  var movieURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-    axios.get(movieURL).then(function(response) {
-      // Place the response.data into a variable, jsonData.
-      var jsonData = response.data;
-  
-      var movieData = [
-        "Title: " + jsonData.Title,
-        "Release Date: " + jsonData.Year,
-        "Rating: " + jsonData.Rated,
-        "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value, 
-        "Country Produce: " + jsonData.Country,
-        "Language: " + jsonData.Language,
-        "Plot: " + jsonData.Plot,
-        "Actors: " + jsonData.Actors
-      ].join("\n\n");
+  axios.get(movieURL).then(function (response) {
+    // Place the response.data into a variable, jsonData.
+    var jsonData = response.data;
 
-      // Append showData and the divider to log.txt, print showData to the console
-      fs.appendFile("log.txt", movieData + divider, function(err) {
-        if (err) throw err;
-        console.log(movieData);
+    var movieData = [
+      "Title: " + jsonData.Title,
+      "Release Date: " + jsonData.Year,
+      "Rating: " + jsonData.Rated,
+      "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value,
+      "Country Produce: " + jsonData.Country,
+      "Language: " + jsonData.Language,
+      "Plot: " + jsonData.Plot,
+      "Actors: " + jsonData.Actors
+    ].join("\n\n");
+
+    // Append showData and the divider to log.txt, print showData to the console
+    fs.appendFile("log.txt", movieData + divider, function (err) {
+      if (err) {
+      console.log("Error: " + err);
+    } else {
+        console.log("Content Added");
       });
-    });
+  });
 };
 
 
