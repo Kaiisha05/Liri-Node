e1c3236b937262e9364064edc52d23e27ccf9730
Kaiisha05 <Kaiisha@yahoo.com>
2019-11-05 20:42:38 -0500

    bands in town api working

--- liri.js
+++ liri.js
@@ -33,83 +33,87 @@ var songAPI = function (song) {
         //  ].join("\n\n");
       } else {
         // for (var s = 0; s > data.tracks.items.length; s++) {
-          let spotifyData = data.tracks.items[0];
-          // console.log(musicData);
-          console.log("Artist: " + spotifyData.artists[0].name);
-          console.log("Song's Name: " + spotifyData.name);
-          console.log("Link: " + spotifyData.preview_url);
-          console.log("Album: " + spotifyData.album.name);
+        let spotifyData = data.tracks.items[0];
+        // console.log(musicData);
+        console.log("Artist: " + spotifyData.artists[0].name);
+        console.log("Song's Name: " + spotifyData.name);
+        console.log("Link: " + spotifyData.preview_url);
+        console.log("Album: " + spotifyData.album.name);
         // }
       }
     });
-  };
+};
 
 
-  // var concertSearch = function (concert) {
-  //   var divider = "\n------------------------------------------------------------\n\n";
+var concertSearch = function (artist) {
+  var divider = "\n------------------------------------------------------------\n\n";
 
-  //   var concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  var concertURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
-  //   axios.get(concertURL).then(function (response) {
-  //     console.log(response)
-  //     var concertData = response.data[0];
+  axios.get(concertURL).then(function (response) {
 
-  //     var concertData = [
-  //       "Venue: " + concertData.offers.venue.name,
-  //       "Location: " + concertData.offers.venue.city,
-  //       "Date: " + concertData.datetime,
-  //     ].join("\n\n");
+    // console.log("this works")
+    var concertData = response.data[0];
+    // console.log(concertData);
+    var bandsData = [
+      "Venue: " + concertData.venue.name,
+      "Location: " + concertData.venue.city + "," + concertData.venue.region,
+      "Date: " + concertData.datetime
+    ].join("\n\n");
+console.log(bandsData);
+//     fs.appendFile("log.txt", bandsData + divider,
+//       function (err) {
+//         if (err) throw err;
+//         console.log("Error: ", err);
+//       })
+  })
+};
 
-  //     fs.appendFile("log.txt", concertData + divider,
-  //       function (err) {
-  //         if (err) throw err;
-  //         console.log(concertData);
-  //       })
-  //   })
-  // };
+var searchMovie = function (movie) {
+  // divider will be used as a spacer between the tv data we print in log.txt
+  var divider = "\n------------------------------------------------------------\n\n";
 
-  var searchMovie = function (movie) {
-    // divider will be used as a spacer between the tv data we print in log.txt
-    var divider = "\n------------------------------------------------------------\n\n";
+  var movieURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-    var movieURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+  axios.get(movieURL).then(function (response) {
+    // Place the response.data into a variable, jsonData.
+    var jsonData = response.data;
 
-    axios.get(movieURL).then(function (response) {
-      // Place the response.data into a variable, jsonData.
-      var jsonData = response.data;
+    var movieData = [
+      "Title: " + jsonData.Title,
+      "Release Date: " + jsonData.Year,
+      "Rating: " + jsonData.Rated,
+      "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value,
+      "Country Produce: " + jsonData.Country,
+      "Language: " + jsonData.Language,
+      "Plot: " + jsonData.Plot,
+      "Actors: " + jsonData.Actors
+    ].join("\n\n");
 
-      var movieData = [
-        "Title: " + jsonData.Title,
-        "Release Date: " + jsonData.Year,
-        "Rating: " + jsonData.Rated,
-        "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value,
-        "Country Produce: " + jsonData.Country,
-        "Language: " + jsonData.Language,
-        "Plot: " + jsonData.Plot,
-        "Actors: " + jsonData.Actors
-      ].join("\n\n");
-
-      // Append showData and the divider to log.txt, print showData to the console
-      fs.appendFile("log.txt", movieData + divider, function (err) {
-        if (err) {
-          console.log("Error: " + err);
-        } else {
-          console.log(movieData);
-        };
-      });
+    // Append showData and the divider to log.txt, print showData to the console
+    fs.appendFile("log.txt", movieData + divider, function (err) {
+      if (err) {
+        console.log("Error: " + err);
+      } else {
+        console.log(movieData);
+      };
     });
-  };
+  });
+};
 
 
-  var command = process.argv[2];
-  // console.log("Input Command:" + command)
+var command = process.argv[2];
+// console.log("Input Command:" + command)
 
-  var userInput = process.argv.slice(3).join(" ");
-  // console.log("User Input: " + userInput)
+var userInput = process.argv.slice(3).join(" ");
+// console.log("User Input: " + userInput)
 
-  if (command === `movie-this`) {
-    searchMovie(userInput)
-  }
-  else if (command === `spotify-this-song`) {
-    songAPI(userInput)
-  }
+if (command === `movie-this`) {
+  searchMovie(userInput)
+}
+else if (command === `spotify-this-song`) {
+  songAPI(userInput)
+}
+else if (command === `concert-this`) {
+  concertSearch(userInput)
+};
